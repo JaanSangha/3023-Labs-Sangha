ca626506812ad847fef09100be3a1cf4b995348f
JaanSangha <jaansangha10@gmail.com>
2021-09-28 22:07:19 -0400

    added tilemap and lights

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,10 +7,13 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 1.0f;
 
+    [SerializeField]
+    private Rigidbody2D rigidbody = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Wrld");
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +22,15 @@ public class PlayerBehaviour : MonoBehaviour
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         transform.position += new Vector3(inputX * moveSpeed * Time.deltaTime, inputY * moveSpeed * Time.deltaTime, 0);
+
+        //rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
     }
+
+    //void FixedUpdate()
+    //{
+    //    float inputX = Input.GetAxisRaw("Horizontal");
+    //    float inputY = Input.GetAxisRaw("Vertical");
+
+    //    rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
+    //}
 }
